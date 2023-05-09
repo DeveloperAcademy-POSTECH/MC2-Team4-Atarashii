3f4c7ee5f9256f63f246f60d5cd7b0730eda5ffd
Jaewon Lee <jwlee010222@gmail.com>
2023-05-09 14:32:49 +0900

    proj change

---
+++ PocketLearner/PocketLearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketLearnerApp.swift
+//  PocketLearner
+//
+//  Created by 이재원 on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct PocketLearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
