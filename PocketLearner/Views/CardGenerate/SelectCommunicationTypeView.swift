73d312407dc718a18de8b5b598a691e90ba5067b
earlysummer0303 <jw9914@naver.com>
2023-05-13 05:31:06 +0900

    <feat>:SelectCommunicationTypeView 한개의 카드만 Flip 될 수 있는 로직 추가 완료.

--- PocketLearner/Views/CardGenerate/SelectCommunicationTypeView.swift
+++ PocketLearner/Views/CardGenerate/SelectCommunicationTypeView.swift
@@ -19,6 +19,7 @@ struct SelectCommunicationTypeView: View {
     // 카드 Rotation 관련 변수
     @State var isFlipped = false
     let durationAndDelay : CGFloat = 0.1
+    
     // 카드 데이터 관련 변수
     @State var fourTypeCardsDatas : [FourTypeCardData] = [
         FourTypeCardData(title: "Analytical", englishDescription: "Fact-Based Introvert", description: "결과보다는 관계와 과정을,\n리스크 보다는 안정감을 중요시해요.", imageTitle: "analyticalCardImage"),
@@ -27,18 +28,21 @@ struct SelectCommunicationTypeView: View {
         FourTypeCardData(title: "Expressive", englishDescription: "Relationship-Based Extrovert", description: "활발하게 소통하고, 창의적이에요\n팀원들간의 화합과 설득을 중시해요.", imageTitle: "expressiveCardImage")
     ]
     
+    // 선택된 카드
+    @State var selectedTypeIndex : Int? = nil
+    
     //카드 Rotation 관련 함수
     func flipCard(index: Int) {
         fourTypeCardsDatas[index].isFlipped.toggle()
         // fourTypeCardsDatas[index].isFlipped = !fourTypeCardsDatas[index].isFlipped
-        if fourTypeCardsDatas[index].isFlipped {
+        if fourTypeCardsDatas[index].isFlipped { // isFlipped = true 일때
             withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
                 fourTypeCardsDatas[index].backDegree = 0
             }
             withAnimation(.linear(duration: durationAndDelay)) {
                 fourTypeCardsDatas[index].frontDegree = 90
             }
-        } else {
+        } else { // isFlipped = true 일때
             withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
                 fourTypeCardsDatas[index].frontDegree = 0
             }
@@ -56,7 +60,7 @@ struct SelectCommunicationTypeView: View {
     }
     var body: some View {
         VStack(spacing: 0) {
-            CardGenerateViewHeader(activatedCircleNumber: activatedCircleNumber, headerTitleMessage: headerTitleMessage, isHeaderDescriptionVisible: isHeaderDescriptionVisible, headerDescriptionMessage:headerDescriptionMessage)
+            CardGenerateViewHeader(activatedCircleNumber: activatedCircleNumber, headerTitleMessage: headerTitleMessage, isHeaderDescriptionVisible: isHeaderDescriptionVisible, headerDescriptionMessage: headerDescriptionMessage)
             
             ZStack(alignment: .center){ // Cards + Coordinate View
                 
@@ -74,6 +78,13 @@ struct SelectCommunicationTypeView: View {
                                 CommunicationStyleCardFront(degree:$fourTypeCardsDatas[0].frontDegree, typeTitle:$fourTypeCardsDatas[0].title, typeEnglishDescription: $fourTypeCardsDatas[0].englishDescription, typeDescription: $fourTypeCardsDatas[0].description, typeImageTitle: $fourTypeCardsDatas[0].imageTitle)
                             }.onTapGesture {
                                 // 탭했을때의 flip 제스쳐
+                                // 만약 이 인덱스가 아닌 다른 인덱스의 카드가 flip => 이걸 flip할 때 그 카드를 unflip
+                                if self.selectedTypeIndex != 0 && self.selectedTypeIndex != nil{
+                                    flipCard(index: selectedTypeIndex!)
+                                }
+                                if self.selectedTypeIndex != 0 {
+                                    self.selectedTypeIndex = 0
+                                } else {self.selectedTypeIndex = nil}
                                 flipCard(index: 0)
                             }
 
@@ -83,7 +94,13 @@ struct SelectCommunicationTypeView: View {
                                 CommunicationStyleCardFront(degree:$fourTypeCardsDatas[1].frontDegree, typeTitle:$fourTypeCardsDatas[1].title, typeEnglishDescription: $fourTypeCardsDatas[1].englishDescription, typeDescription: $fourTypeCardsDatas[1].description, typeImageTitle: $fourTypeCardsDatas[1].imageTitle)
                             }.onTapGesture {
                                     // 탭했을때의 flip 제스쳐
-                                    flipCard(index: 1)
+                                if self.selectedTypeIndex != 1 && self.selectedTypeIndex != nil{
+                                    flipCard(index: selectedTypeIndex!)
+                                }
+                                if self.selectedTypeIndex != 1 {
+                                    self.selectedTypeIndex = 1
+                                } else {self.selectedTypeIndex = nil}
+                                flipCard(index: 1)
                             }
                         }
                     HStack(spacing: 23.9) { // Amiable, Expressive
@@ -94,7 +111,14 @@ struct SelectCommunicationTypeView: View {
                                     degree: $fourTypeCardsDatas[2].frontDegree, typeTitle: $fourTypeCardsDatas[2].title, typeEnglishDescription: $fourTypeCardsDatas[2].englishDescription, typeDescription: $fourTypeCardsDatas[2].description, typeImageTitle: $fourTypeCardsDatas[2].imageTitle)
                             }.onTapGesture {
                                 // 탭했을때의 flip 제스쳐
+                                if self.selectedTypeIndex != 2 && self.selectedTypeIndex != nil{
+                                    flipCard(index: selectedTypeIndex!)
+                                }
+                                if self.selectedTypeIndex != 2 {
+                                    self.selectedTypeIndex = 2
+                                } else {self.selectedTypeIndex = nil}
                                 flipCard(index: 2)
+                                    
                             }
 
                             ZStack {
@@ -104,7 +128,14 @@ struct SelectCommunicationTypeView: View {
                                     degree: $fourTypeCardsDatas[3].frontDegree, typeTitle: $fourTypeCardsDatas[3].title, typeEnglishDescription: $fourTypeCardsDatas[3].englishDescription, typeDescription: $fourTypeCardsDatas[3].description, typeImageTitle: $fourTypeCardsDatas[3].imageTitle)
                             }.onTapGesture {
                                     // 탭했을때의 flip 제스쳐
-                                    flipCard(index: 3)
+                                if self.selectedTypeIndex != 3 && self.selectedTypeIndex != nil{
+                                    flipCard(index: selectedTypeIndex!)
+                                }
+                                if self.selectedTypeIndex != 3 {
+                                    self.selectedTypeIndex = 3
+                                } else {self.selectedTypeIndex = nil}
+                                flipCard(index: 3)
+                                    
                             }
                         }
                    
