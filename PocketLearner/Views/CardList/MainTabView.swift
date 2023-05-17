ecfc0e352e9d9ae915c8d48ab9669d63c8c08a9c
220v-K <jwlee010222@gmail.com>
2023-05-17 16:47:41 +0900

    탭바 임시 삭제(팀 기능 구현 x)

--- PocketLearner/Views/CardList/MainTabView.swift
+++ PocketLearner/Views/CardList/MainTabView.swift
@@ -11,26 +11,26 @@ struct MainTabView: View {
     
     var body: some View {
         NavigationStack {
-            TabView {
+//            TabView {
                 // MARK: - 명함 탭 연결
                 MainNameCardTabView()
-                    .tabItem {
-                        Image(systemName: "person.text.rectangle")
-                        Text("First")
-                    }
+//                    .tabItem {
+//                        Image(systemName: "person.text.rectangle")
+//                        Text("First")
+//                    }
                 
                 // MARK: - 팀 탭 연결
                 /// TODO: 팀 탭 생성 및 연결
 //                MainNameCardTabView()
-                TeamCommingSoonView() // 아따라시팀은 공사중 페이지
-                    .tabItem {
-                        Image(systemName: "person.3.sequence")
-                        Text("Second")
-                    }
-            }
-            .onAppear {
-                UITabBar.appearance().backgroundColor = .white
-            }
+//                TeamCommingSoonView() // 아따라시팀은 공사중 페이지
+//                    .tabItem {
+//                        Image(systemName: "person.3.sequence")
+//                        Text("Second")
+//                    }
+//            }
+//            .onAppear {
+//                UITabBar.appearance().backgroundColor = .white
+//            }
         }
     }
 }
