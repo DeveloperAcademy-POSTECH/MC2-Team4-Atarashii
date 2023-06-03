cc1af1426915e42bd92dc0d9aec6e0d56a5cd8ab
220v-K <jwlee010222@gmail.com>
2023-06-03 14:21:22 +0900

    <feat, add> : Cloud Function - make JWT function / add authKey (Sign in with Apple)

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,32 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+function makeJWT() {
+  const jwt = require("jsonwebtoken");
+  const fs = require("fs");
+
+  // Path to download key file from developer.apple.com/account/resources/authkeys/list
+  let privateKey = fs.readFileSync("AuthKey_XXXXXXXXXX.p8");
+
+  //Sign with your team ID and key ID information.
+  let token = jwt.sign(
+    {
+      iss: "YOUR TEAM ID",
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 120,
+      aud: "https://appleid.apple.com",
+      sub: "YOUR CLIENT ID",
+    },
+    privateKey,
+    {
+      algorithm: "ES256",
+      header: {
+        alg: "ES256",
+        kid: "YOUR KEY ID",
+      },
+    }
+  );
+
+  return token;
+}
