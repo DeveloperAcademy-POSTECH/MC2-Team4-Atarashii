e4948fdc922f40b7e00112094a166eb40c0163f4
earlysummer0303 <jw9914@naver.com>
2023-08-17 22:30:21 +0900

    refactor: 폴더 구조 리팩토링 및 Assets 파일 생성

---
+++ PocketLearner/TestView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Image(.mainCharacter)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
